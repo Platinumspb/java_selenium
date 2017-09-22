c1a2e5f7b849e9742ca7dc9ff63cf1ec18c7fa7c
Evgeny Kim <kenju999@gmail.com>
2017-09-21 22:13:09 -0700

    new functions

--- src/main/java/com/stqa/java_selenium/HelloWorld.java
+++ src/main/java/com/stqa/java_selenium/HelloWorld.java
@@ -1,8 +1,29 @@
 package com.stqa.java_selenium;
 
-public class HelloWorld {
-    public static void main(String[] args){
+import javax.sound.midi.Soundbank;
 
-        System.out.print("Hello Evgeny!!!!");
+public class HelloWorld {
+  public static void main(String[] args) {
+    hello("man");
+    hello("dude");
+    double len = 5;
+    System.out.println("Area of square with length "+ len + "=" + area(len));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Square triangle a and b = " + area(a,b) );
+
     }
+
+  public static void hello(String somebody){
+
+    System.out.println("Hello, " +somebody+ "!");
+  }
+
+  public static double area(double l){
+    return l*l;
+  }
+  public static double area(double a, double b){
+    return a * b;
+  }
 }
